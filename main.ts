eea1096e30e0412f1116437d004b39a16ace9882
EthanRule <ethan2002@live.com>
2025-10-21 10:54:27 -0700

    switches on commands and initial addTask function

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+// Note: the current implementation of this project does heavy disk reads and writes. Idealy a program fetches the data, manipulates it,
+// then saves on user exit to utilize the ram and cpu cache. But for the sake of practice, this program will be updating the json on disk.
+
+import * as fs from "fs";
+
 enum Progress {
 	todo = "todo",
 	inProgress = "in-progress",
@@ -14,9 +19,19 @@ interface Task {
 }
 
 class TaskTracker {
-	addTask(task: Task): void {}
+	addTask(data: string): void {
+		const task: Task = {
+			id: generateUniqueId(),
+			description: data,
+			status: 'todo',
+			createdAt: getTime(),
+			updatedAt: getTime(),
+		}
+		fs.
 
-	updateTask(): void {
+	}
+
+	updateTask(taskId: number, newTask: string): void {
 		//TODO: args?
 	}
 
@@ -31,6 +46,11 @@ class TaskTracker {
 	listNotDoneTasks(): void {}
 
 	listTasksInProgress(): void {}
+
+	private genearteUniueId(): number {
+		// Parse the currnet json for unique id
+
+	}
 }
 
 function parseInput(data: string): [string[], number] {
@@ -38,7 +58,6 @@ function parseInput(data: string): [string[], number] {
 	// Gather argument values in a list
 	let word = "";
 	for (let i = 0; i < data.length; i++) {
-		console.log(data[i]);
 		if ((data[i] === " " || data[i] == "\n") && word.length > 0) {
 			argv.push(word);
 			word = "";
@@ -51,14 +70,77 @@ function parseInput(data: string): [string[], number] {
 	return [argv, argc];
 }
 
-function main(): void {
+function initializeJSON(): void {
+	if (!fs.existsSync("db.json")) {
+		fs.writeFile("db.json", "", "utf8", (err) => {
+			if (err) {
+				console.log(`Error creating file`);
+			}
+		});
+	}
+}
+
+function isdigit(data: string): boolean {
+	for (let i = 0; i < data.length; i++) {
+		if (!/^\d$/.test(data[i])) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function input(): void {
 	process.stdout.write("task-cli ");
 	process.stdin.once("data", (data) => {
 		const [argv, argc] = parseInput(data.toString());
+		switch (argv[0]) {
+			case "add":
+				taskTracker.addTask(argv[1]);
+				break;
+			case "update":
+				if (isdigit(argv[1])) {
+					taskTracker.updateTask(Number(argv[1]), argv[2]);
+				}
+				break;
+			case "delete":
+				break;
+			case "mark-in-progress":
+				break;
+			case "mark-done":
+				break;
+			case "list":
+				switch (argv[1]) {
+					case "done":
+						break;
+					case "todo":
+						break;
+					case "in-progress":
+						break;
+					default:
+						console.log("Invalid input. For a list of commands use the command: help");
+				}
+				break;
+			case "quit":
+				process.exit();
+			case "exit":
+				process.exit();
+			default:
+				console.log("Invalid input. For a list of commands use the command: help");
+		}
 
-        
-		main();
+		input();
 	});
 }
 
+let taskTracker = new TaskTracker();
+function main(): void {
+	initializeJSON();
+	input();
+}
+
 main();
+// Tests
+
+// Helper functions
+
+function testParseInput() {}
